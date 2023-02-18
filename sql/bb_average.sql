0f560a3c86562b3fb902dfddf66fcf6c24569372
Welbert Castro <welberthime@gmail.com>
2023-02-18 00:07:19 -0300

    Remove not used files

--- sql/bb_average.sql
+++
@@ -1,7 +0,0 @@
-SELECT EXTRACT(WEEK FROM datetime), COUNT(1)
-FROM jobsitytrips.refined_trips
-WHERE ST_COVERS(
-    ST_GEOGFROMTEXT('POLYGON(({} {}, {} {}, {} {}, {} {}, {} {}))'), 
-    destination_coord
-  )
-GROUP BY EXTRACT(WEEK FROM datetime);
--- sql/region_average.sql
+++
@@ -1,4 +0,0 @@
-SELECT EXTRACT(WEEK FROM datetime), COUNT(1)
-FROM jobsitytrips.refined_trips
-WHERE region = '{}'
-GROUP BY EXTRACT(WEEK FROM datetime)
