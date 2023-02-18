6d16c086b2bfffde0529171ca455419ef3350b24
Welbert Castro <welberthime@gmail.com>
2023-02-18 00:34:40 -0300

    Add python scripts

---
+++ scripts/push_data.py
@@ -0,0 +1,25 @@
+import requests
+import subprocess
+import pandas as pd
+
+
+_URL = "https://us-central1-trip-analysis-18022023.cloudfunctions.net/new-trip"
+gcloud_auth = subprocess.Popen(
+        args="gcloud auth print-identity-token", 
+        stdout=subprocess.PIPE, 
+        shell=True
+    ).communicate()
+token = str(gcloud_auth[0])[2:-3]
+
+def make_request(message, token):
+    print("sending message...")
+    res = requests.post(
+        _URL, 
+        json=message, 
+        headers={"Authorization": f"Bearer {token}"}
+    )
+    print(res.json())
+
+df = pd.read_csv("./scripts/trips.csv")
+for record in df.to_dict("records"):
+    make_request(record, token)
---
+++ scripts/query.py
@@ -0,0 +1,46 @@
+import requests
+import subprocess
+import json
+
+
+_URL = "https://us-central1-trip-analysis-18022023.cloudfunctions.net/average"
+gcloud_auth = subprocess.Popen(
+    args="gcloud auth print-identity-token", 
+    stdout=subprocess.PIPE, 
+    shell=True
+).communicate()
+token = str(gcloud_auth[0])[2:-3]
+
+def test_region(token):
+    params = {
+        "type": "region",
+        "region": "Prague",
+    }
+    print("requesting region query...")
+    res = requests.post(
+        _URL, 
+        json=params, 
+        headers={"Authorization": f"Bearer {token}"}
+    )
+    print(res)
+    print(res.json())
+
+def test_bounding_box(token):
+    params = {
+        "type": "bounding_box",
+        "upper_left": "0 0",
+        "upper_right": "0 90",
+        "lower_left": "90 90",
+        "lower_right": "90 0",
+    }
+    print("requesting bounding box query...")
+    res = requests.post(
+        _URL, 
+        json=params, 
+        headers={"Authorization": f"Bearer {token}"}
+    )
+    print(res)
+    print(res.json())
+
+test_region(token)
+test_bounding_box(token)
